8d364ae044cfd53cfccbb11cb9b175efd779f647
miyazaki <miya.taka.0096@gmail.com>
2024-08-04 16:12:54 +0900

    Chapter4 #1

---
+++ backend/api/hello_db/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Hello
+
+# Create your views here.
+class Db(APIView):
+  def get(self, repuest, format=None):
+    entry = Hello.objects.get(id=1)
+    return Response({"message": entry.world})
